6de463918d58b1989b2a419ac2a3d05ae7ef04ba
Anoop Raj <anoopraj.dev@gmail.com>
2025-10-01 03:30:48 +0530

    User signin ui & api completion

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import userRoutes from './routes/user.Routes.js';
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
 const app = express();
+
 
 //middlewares
 app.use(express.json())
 app.use(cors());
+app.use(cookieParser())
 
 //routes
 app.use('/api/auth',userRoutes)
--- backend/src/routes/user.Routes.ts
+++ backend/src/routes/user.Routes.ts
@@ -2,11 +2,13 @@ import {Router} from 'express';
 import { userSignup } from '../controllers/authControllers/userSignup.controller.js';
 import { generateId } from '../middlewares/registrationID.js';
 import { verifyOtp } from '../controllers/authControllers/verifyOtp.controller.js';
+import { userSignin } from '../controllers/authControllers/userSignin.controller.js';
 
 const router = Router();
 
 //signup route
 router.post('/signup',generateId(),userSignup);
+router.post('/signin',userSignin)
 
 //email verification
 router.post('/verify-email',verifyOtp)
